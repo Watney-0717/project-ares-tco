fa31d9a33d9f9f24887d76098fa378e967d04c1c
Watney-0717 <nanasinotarou@proton.me>
2026-08-13 17:34:47 +0900

    Update router.py

--- src/router.py
+++ src/router.py
@@ -1,4 +1,4 @@
-python# Copyright 2026 Watney-0717
+# Copyright 2026 Watney-0717
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
